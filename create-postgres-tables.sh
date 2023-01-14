2fd7cc2ee64bd33603c84571094f5fe5a97981f4
Koki Ueha <jeamsblue@gmail.com>
2023-01-14 15:15:45 +0000

    Fix test table definition

--- create-postgres-tables.sh
+++ create-postgres-tables.sh
@@ -44,7 +44,8 @@ insert into cars(brand, model, color)
 
 create table intervals as select '42 day'::INTERVAL interval_days UNION ALL SELECT '42 month'::INTERVAL UNION ALL SELECT '42 year'::INTERVAL UNION ALL SELECT  '42 minute'::INTERVAL UNION ALL SELECT  '42 second'::INTERVAL UNION ALL SELECT '0.42 second'::INTERVAL UNION ALL SELECT '-42 day'::INTERVAL interval_days  UNION ALL SELECT NULL::INTERVAL;
 
-create table numerics as select n from (values (32.8875000), (-32.8875000), (10000), (1000), (100), (10), (1), (0.1), (0.01), (0.001), (0.0001), (0.00001), (0.000001), (0.0000001), (10001), (1.0001), (0.0001001), (10001.0001), (1.0001001), (10001.0001001), (0)) t(n);
+create table numerics (n numeric(12, 7));
+insert into numerics values (32.8875000), (-32.8875000), (10000), (1000), (100), (10), (1), (0.1), (0.01), (0.001), (0.0001), (0.00001), (0.000001), (0.0000001), (10001), (1.0001), (0.0001001), (10001.0001), (1.0001001), (10001.0001001), (0);
 " |  psql -d postgresscanner
 
 

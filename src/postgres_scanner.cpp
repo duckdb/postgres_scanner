603b26de6933a883933b9c52f251b449a2cb7533
Mark Raasveldt <mark.raasveldt@gmail.com>
2023-10-10 12:24:36 +0200

    Scanner cleanup - move filter pushdown to separate file/class

--- src/postgres_scanner.cpp
+++ src/postgres_scanner.cpp
@@ -4,9 +4,7 @@
 
 #include "duckdb/main/extension_util.hpp"
 #include "duckdb/parser/parsed_data/create_table_function_info.hpp"
-#include "duckdb/planner/table_filter.hpp"
-#include "duckdb/planner/filter/conjunction_filter.hpp"
-#include "duckdb/planner/filter/constant_filter.hpp"
+#include "postgres_filter_pushdown.hpp"
 #include "postgres_scanner.hpp"
 #include "postgres_result.hpp"
 #include "postgres_conversion.hpp"
@@ -226,61 +224,6 @@ static unique_ptr<FunctionData> PostgresBind(ClientContext &context, TableFuncti
 	return std::move(bind_data);
 }
 
-static string TransformFilter(string &column_name, TableFilter &filter);
-
-static string CreateExpression(string &column_name, vector<unique_ptr<TableFilter>> &filters, string op) {
-	vector<string> filter_entries;
-	for (auto &filter : filters) {
-		filter_entries.push_back(TransformFilter(column_name, *filter));
-	}
-	return "(" + StringUtil::Join(filter_entries, " " + op + " ") + ")";
-}
-
-static string TransformComparision(ExpressionType type) {
-	switch (type) {
-	case ExpressionType::COMPARE_EQUAL:
-		return "=";
-	case ExpressionType::COMPARE_NOTEQUAL:
-		return "!=";
-	case ExpressionType::COMPARE_LESSTHAN:
-		return "<";
-	case ExpressionType::COMPARE_GREATERTHAN:
-		return ">";
-	case ExpressionType::COMPARE_LESSTHANOREQUALTO:
-		return "<=";
-	case ExpressionType::COMPARE_GREATERTHANOREQUALTO:
-		return ">=";
-	default:
-		throw NotImplementedException("Unsupported expression type");
-	}
-}
-
-static string TransformFilter(string &column_name, TableFilter &filter) {
-	switch (filter.filter_type) {
-	case TableFilterType::IS_NULL:
-		return column_name + " IS NULL";
-	case TableFilterType::IS_NOT_NULL:
-		return column_name + " IS NOT NULL";
-	case TableFilterType::CONJUNCTION_AND: {
-		auto &conjunction_filter = (ConjunctionAndFilter &)filter;
-		return CreateExpression(column_name, conjunction_filter.child_filters, "AND");
-	}
-	case TableFilterType::CONJUNCTION_OR: {
-		auto &conjunction_filter = (ConjunctionAndFilter &)filter;
-		return CreateExpression(column_name, conjunction_filter.child_filters, "OR");
-	}
-	case TableFilterType::CONSTANT_COMPARISON: {
-		auto &constant_filter = (ConstantFilter &)filter;
-		// TODO properly escape ' in constant value
-		auto constant_string = "'" + constant_filter.constant.ToString() + "'";
-		auto operator_string = TransformComparision(constant_filter.comparison_type);
-		return StringUtil::Format("%s %s %s", column_name, operator_string, constant_string);
-	}
-	default:
-		throw InternalException("Unsupported table filter type");
-	}
-}
-
 static void PostgresInitInternal(ClientContext &context, const PostgresBindData *bind_data_p,
                                  PostgresLocalState &lstate, idx_t task_min, idx_t task_max) {
 	D_ASSERT(bind_data_p);
@@ -303,16 +246,7 @@ static void PostgresInitInternal(ClientContext &context, const PostgresBindData 
 		}
 	}
 
-	string filter_string;
-	if (lstate.filters && !lstate.filters->filters.empty()) {
-		vector<string> filter_entries;
-		for (auto &entry : lstate.filters->filters) {
-			auto column_name = KeywordHelper::WriteQuoted(bind_data->names[lstate.column_ids[entry.first]], '"');
-			auto &filter = *entry.second;
-			filter_entries.push_back(TransformFilter(column_name, filter));
-		}
-		filter_string = " AND " + StringUtil::Join(filter_entries, " AND ");
-	}
+	string filter_string= PostgresFilterPushdown::TransformFilters(lstate.column_ids, lstate.filters, bind_data->names);
 
 	lstate.sql = StringUtil::Format(
 	    R"(

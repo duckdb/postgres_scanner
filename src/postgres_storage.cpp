6541bd846775865326dec0cd5e63b7e8a733f2fc
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-04-19 15:17:47 +0200

    Allow default secrets to be used when no secret is specified, and handle conflicts between temporary and persistent secrets

--- src/postgres_storage.cpp
+++ src/postgres_storage.cpp
@@ -37,36 +37,61 @@ string AddConnectionOption(const KeyValueSecret &kv_secret, const string &name) 
 	return result;
 }
 
+unique_ptr<SecretEntry> GetSecret(ClientContext &context, const string &secret_name) {
+	auto &secret_manager = SecretManager::Get(context);
+	auto transaction = CatalogTransaction::GetSystemCatalogTransaction(context);
+	// FIXME: this should be adjusted once the `GetSecretByName` API supports this use case
+	auto secret_entry = secret_manager.GetSecretByName(transaction, secret_name, "memory");
+	if (secret_entry) {
+		return secret_entry;
+	}
+	secret_entry = secret_manager.GetSecretByName(transaction, secret_name, "local_file");
+	if (secret_entry) {
+		return secret_entry;
+	}
+	return nullptr;
+}
+
 static unique_ptr<Catalog> PostgresAttach(StorageExtensionInfo *storage_info, ClientContext &context,
                                           AttachedDatabase &db, const string &name, AttachInfo &info,
                                           AccessMode access_mode) {
 	string connection_string = info.path;
+
+	string secret_name;
 	for(auto &entry : info.options) {
 		auto lower_name = StringUtil::Lower(entry.first);
 		if (lower_name == "type" || lower_name == "read_only") {
 			// already handled
 		} else if (lower_name == "secret") {
-			auto &secret_manager = SecretManager::Get(context);
-			auto transaction = CatalogTransaction::GetSystemCatalogTransaction(context);
-			auto secret_name = entry.second.ToString();
-			auto secret_entry = secret_manager.GetSecretByName(transaction, secret_name);
-			if (!secret_entry) {
-				throw BinderException("Secret with name \"%s\" not found", secret_name);
-			}
-			// secret found - read
-			const auto &kv_secret = dynamic_cast<const KeyValueSecret &>(*secret_entry->secret);
-			string new_connection_info;
-
-			new_connection_info += AddConnectionOption(kv_secret, "user");
-			new_connection_info += AddConnectionOption(kv_secret, "password");
-			new_connection_info += AddConnectionOption(kv_secret, "host");
-			new_connection_info += AddConnectionOption(kv_secret, "port");
-			new_connection_info += AddConnectionOption(kv_secret, "dbname");
-
-			connection_string = new_connection_info + connection_string;
+			secret_name = entry.second.ToString();
 		} else {
 			throw BinderException("Unrecognized option for Postgres attach: %s", entry.first);
 		}
+	}
+
+	// if no secret is specified we default to the unnamed postgres secret, if it exists
+	bool explicit_secret = !secret_name.empty();
+	if (!explicit_secret) {
+		// look up settings from the default unnamed postgres secret if none is provided
+		secret_name = "__default_postgres";
+	}
+
+	auto secret_entry = GetSecret(context, secret_name);
+	if (secret_entry) {
+		// secret found - read data
+		const auto &kv_secret = dynamic_cast<const KeyValueSecret &>(*secret_entry->secret);
+		string new_connection_info;
+
+		new_connection_info += AddConnectionOption(kv_secret, "user");
+		new_connection_info += AddConnectionOption(kv_secret, "password");
+		new_connection_info += AddConnectionOption(kv_secret, "host");
+		new_connection_info += AddConnectionOption(kv_secret, "port");
+		new_connection_info += AddConnectionOption(kv_secret, "dbname");
+
+		connection_string = new_connection_info + connection_string;
+	} else if (explicit_secret) {
+		// secret not found and one was explicitly provided - throw an error
+		throw BinderException("Secret with name \"%s\" not found", secret_name);
 	}
 	return make_uniq<PostgresCatalog>(db, connection_string, access_mode);
 }

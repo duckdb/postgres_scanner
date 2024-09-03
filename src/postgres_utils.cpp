2c9490250769a7dcb4fb0c411705fbca042720a2
Mark Raasveldt <mark.raasveldt@gmail.com>
2024-09-03 13:50:56 +0200

    Fix  #246 - convert ubigint to decimal(20,0) instead of to double to avoid loss of precision

--- src/postgres_utils.cpp
+++ src/postgres_utils.cpp
@@ -253,6 +253,7 @@ LogicalType PostgresUtils::ToPostgresType(const LogicalType &input) {
 	case LogicalTypeId::UINTEGER:
 		return LogicalType::BIGINT;
 	case LogicalTypeId::UBIGINT:
+          return LogicalType::DECIMAL(20, 0);
 	case LogicalTypeId::HUGEINT:
 		return LogicalType::DOUBLE;
 	default:

5d0108835179762202dca2a5deeb4a0e4ff3461d
Alex Kasko <alex@staticlibs.net>
2026-09-11 23:04:20 +0100

    Use transaction manager from common repo (1.5)
    This is a backport of the PR #548 to `v1.5-variegata` stable branch.

--- src/include/storage/postgres_transaction_manager.hpp
+++ src/include/storage/postgres_transaction_manager.hpp
@@ -8,29 +8,13 @@
 
 #pragma once
 
-#include "duckdb/transaction/transaction_manager.hpp"
+#include "dbconnector/storage/transaction_manager.hpp"
+
 #include "storage/postgres_catalog.hpp"
 #include "storage/postgres_transaction.hpp"
-#include "duckdb/common/reference_map.hpp"
 
 namespace duckdb {
 
-class PostgresTransactionManager : public TransactionManager {
-public:
-	PostgresTransactionManager(AttachedDatabase &db_p, PostgresCatalog &postgres_catalog);
-
-	Transaction &StartTransaction(ClientContext &context) override;
-	ErrorData CommitTransaction(ClientContext &context, Transaction &transaction) override;
-	void RollbackTransaction(Transaction &transaction) override;
-
-	void Checkpoint(ClientContext &context, bool force = false) override;
-
-private:
-	void DestroyTransaction(Transaction &transaction);
-
-	PostgresCatalog &postgres_catalog;
-	mutex transaction_lock;
-	reference_map_t<Transaction, unique_ptr<PostgresTransaction>> transactions;
-};
+using PostgresTransactionManager = dbconnector::storage::TransactionManager<PostgresCatalog, PostgresTransaction>;
 
 } // namespace duckdb
